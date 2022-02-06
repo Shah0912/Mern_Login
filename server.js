575be36bbbc368b9e246c99a4041c589cb108f27
Aditya Shah <adi.shah0912@gmail.com>
2022-02-06 17:38:54 +0530

    simple server created

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
